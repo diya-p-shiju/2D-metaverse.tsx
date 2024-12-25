0e82bef6cd0301baa9d70c9c2d1fca0ae7e8d2fd
Diya Pullekkattil Shiju <diya-p-shiju@gmail.com>
2024-12-26 04:13:40 +0530

    first commit after changing to tsx

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+
+const HomePage = () => {
+  return (
+    <>
+   <div className="bg-background">hey</div>
+    <Button variant="default">hello</Button>
+    </>
+  )
+}
+
+export default HomePage
